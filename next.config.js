0b4923b75ad064a3b2d3e947c63b383420757a34
Cyril26 <cyrilodm26@gmail.com>
2023-10-09 16:36:27 +0000

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["tewobyjtziwzahdsrzjl.supabase.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
